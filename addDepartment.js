683f1908530c04de79efee2f2218861ce13a6fdc
lynn chiang <lynnchiang@mitac.com.tw>
2023-12-14 17:02:34 +0800

    addEmp js
    addEmp js

--- addDepartment.js
+++ addDepartment.js
@@ -1,22 +1,59 @@
+//===== 取得元素 =====
+let inputDeptName_el = $("#inputDeptName");
+let inputDeptLoc_el = $("#inputDeptLoc");
+
+//===== 錯誤處理 =====
+let inputDeptName = $("#inputDeptName");
+let inputDeptLoc = $("#inputDeptLoc");
+
+//===== 判斷錯誤處理新增的文字 =====
+let warnStr = $("<span>").css("color", "red");
+
+//===== 初始化 =====
+inputDeptName.html('');
+inputDeptLoc.html('');
+
+
+
 $(document).ready(function(){
 
-    $("#deptSubmitBtn").click(function(){
-        //取得表單input資料
-        var inputDeptName_el = $("#inputDeptName").val();
-        var inputDeptLoc_el = $("#inputDeptLoc").val();
+    $("#deptSubmitBtn").on('click', function(e){
 
-        console.log("XXX");
+        //將form表單預設行為先關閉
+        e.preventDefault();
+        let control = true;
+
+        //按下Btn時先做輸入格式等錯誤處理
+        if(inputDeptName_el.val() === null || inputDeptName_el.val().trim() === ""){
+            control = false;
+            warnStr.html("請填寫部門名稱");
+            return;
+        }
+        if(inputDeptLoc_el.val() === null || inputDeptLoc_el.val().trim() === ""){
+            control = false;
+            warnStr.html("請填寫部門地點");
+            return;
+        }
+
+        //取得表單input資料
+        let deptNameValue = inputDeptName_el.val();
+        let deptLocValue = inputDeptLoc_el.val();
+
+        let data = {
+            deptName:deptNameValue,  //前 物件名稱 : 後 取到的值
+            loc:deptLocValue
+        }
+  
+
         //使用AJAX發送請求
         $.ajax({
             url:'http://localhost:8080/api/addDepartment',
             method:'POST',
-            data:{
-                deptName: inputDeptName_el, //:後是從表單來的值
-                loc: inputDeptLoc_el
-            },
-            success:function(response){
+            contentType: 'application/json',
+            data: JSON.stringify(data),
+            success: function(response){
                 //處理成功的回應
-                console.log('資料送出成功',response);
+                console.log('資料送出成功', response);
             },
             error: function(error){
                 //處理錯誤
